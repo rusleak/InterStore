08a4a1c529b30397be101d2194c28d113a015551
rusleak <hotlinetony2@gmail.com>
2025-02-12 14:26:19 +0100

    Color filter in process / backup 3

--- src/main/java/mainpackage/interstore/repository/OrderItemRepository.java
+++ src/main/java/mainpackage/interstore/repository/OrderItemRepository.java
@@ -1,7 +1,7 @@
 package mainpackage.interstore.repository;
 
 import mainpackage.interstore.model.OrderItem;
-import mainpackage.interstore.util.OrderItemId;
+import mainpackage.interstore.model.util.OrderItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
